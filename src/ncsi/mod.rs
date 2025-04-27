65675a8bb9a93498956f3cf21e9c95b7340b78d9
Björn <bjorn@sentiandream.com>
2025-04-27 21:39:25 +0200

    chore: clean up

--- src/ncsi/mod.rs
+++ src/ncsi/mod.rs
@@ -26,12 +26,6 @@ enum NcsiStatus {
  * Result with a Check status.
  */
 pub async fn run_ncsi() -> Result<ExitCode> {
-    // Step 1: Send a DNS request to resolve the web host.
-    // Step 2: If valid, do a plain HTTP GET.
-    // Step 3: Validate the contents.
-    // Step 4: Send a DNS request to resolve the dns host.
-    // Step 5: Evaluate requests.
-
 //TODO: Return Internet Access, Limited Connectivity or No Internet Access.
 
     debug!("Running NCSI for IPv4");
@@ -41,51 +35,6 @@ pub async fn run_ncsi() -> Result<ExitCode> {
         return Ok(ExitCode::SUCCESS);
     }
     Ok(ExitCode::from(codes::NCSI_OUT_OF_OPTIONS))
-
-//TODO: Learn how to simplify these statements
-//     let web_check = active_web_probing().await?;
-//     if web_check == Check::Success {
-//         debug!("Active web probing succeeded");
-//         return Ok(ExitCode::SUCCESS);
-//     }
-//     debug!("Active web probing failed");
-
-    // let dns_check = active_dns_probing()?;
-    // if dns_check == Check::Success {
-    //     return Ok(ExitCode::SUCCESS);
-    // }
-    //
-    // Ok(ExitCode::from(codes::NCSI_OUT_OF_OPTIONS))
-}
-
-async fn active_web_probing() -> Result<Check> {
-//TODO: Implement
-
-    // Probe IPv4 and IPv6 in parallel
-    // If either succeeds, return success
-
-//TODO: Get addresses
-//     let url_v4_raw = format!("http://{}", MS_WEB_IPV4_HOST);
-//     let url_v4_base = Url::parse(url_v4_raw.as_str())?;
-//     let url_v4 = url_v4_base.join(ms::MS_WEB_IPV4_PATH)?;
-    let url_v4 = Url::parse(ms::MS_WEB_IPV4_URL)?;
-
-    let ipv4check = probing::invoke_web_request(url_v4).await?;
-    debug!("Active web probe result is {:?}", ipv4check);
-    if ipv4check == Check::Success {
-        return Ok(Check::Success);
-    }
-
-    Ok(Check::Failure)
-}
-
-fn active_dns_probing() -> Result<Check> {
-//TODO: Implement
-
-    // Attempt DNS-resolution on IPv4 and IPv6 in parallel
-    // If either succeeds, return success
-
-    Ok(Check::Failure)
 }
 
 async fn probe_ipv4() -> Result<NcsiStatus> {
