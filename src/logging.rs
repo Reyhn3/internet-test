f35d9618627af1d32b59e4ab351ae8f11e9c2831
Björn <bjorn@sentiandream.com>
2026-05-01 17:27:54 +0200

    refactor: logging ansi colors

--- src/logging.rs
+++ src/logging.rs
@@ -1,8 +1,14 @@
+use anstyle::{Ansi256Color, AnsiColor, Color, Style};
 use chrono::Local;
 use log::{debug, error, info, log_enabled, trace, warn, Level, LevelFilter};
 use std::io::Write;
+use anstyle::AnsiColor::{Black, White};
 
-const DATE_FORMAT_STR: &'static str = "\x1b[38;5;244m%H:%M:%S.%f\x1b[0m";
+const DATE_FORMAT_STR: &'static str = "%H:%M:%S.%f";
+static TIMESTAMP_STYLE: Style = Style::new().fg_color(Some(Color::Ansi256(Ansi256Color(242))));
+static LEVEL_STYLE: Style = Style::new()
+    .bg_color(Some(Color::Ansi(White)))
+    .fg_color(Some(Color::Ansi(Black)));
 
 pub fn init(quiet: bool, verbose: bool) {
     if quiet {
@@ -32,7 +38,7 @@ pub fn init(quiet: bool, verbose: bool) {
 
             writeln!(
                 buf,
-                "{timestamp} [{}]{pad}{style}{}{style:#}",
+                "{TIMESTAMP_STYLE}{timestamp}{TIMESTAMP_STYLE:#} {LEVEL_STYLE}{}{LEVEL_STYLE:#}{pad}{style}{}{style:#}",
                 record.level(),
                 record.args()
             )
